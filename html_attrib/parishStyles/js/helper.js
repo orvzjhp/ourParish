f1a999ad68ad9c7484bbb3a78ee9a43e96f301a8
junichi <junichimiyahara3@yahoo.com>
2014-06-13 15:23:31 +0800

    asdfsdf

--- html_attrib/parishStyles/js/helper.js
+++ html_attrib/parishStyles/js/helper.js
@@ -513,11 +513,13 @@ function ServiceSwitcher()
 	this.init = function()
 	{
 		document.getElementById("init").innerHTML = '<meta name ="viewport" content = "width=device-width, initial-scale = 1.0">'+
-		'<link href = "'+ base_url +'html_attrib/parishStyles/services/servbootstrap.css" rel = "stylesheet">"' +
-		'<link href = "'+ base_url +'html_attrib/parishStyles/services/styles.css" rel = "stylesheet">'+
+		'<link href = "'+ base_url +'html_attrib/parishStyles/css/bootstrap_2.css" rel = "stylesheet">' +
+		'<link href = "'+base_url+'html_attrib/parishStyles/css/bootstrap.css" rel = "stylesheet">' +
+		'<link href = "'+ base_url +'html_attrib/parishStyles/css/styles.css" rel = "stylesheet">'+
+		'<link href = "'+ base_url +'html_attrib/parishStyles/css/newStyle.css" rel = "stylesheet">'+
 		'</style><link rel="stylesheet" type="text/css" href="'+ base_url +'html_attrib/parishStyles/css/parishStyle.css" media="screen"></style>';
 	};
-
+	
 	var closure = function(ref, key)
 	{
 		return function()
@@ -563,7 +565,7 @@ function Helper()
 	this.init = function()
 	{
 		var base_url = $("#init").data('base_url');
-		document.getElementById("init").innerHTML = '<meta name ="viewport" content = "width=device-width, initial-scale = 1.0"><link href = "'+ base_url +'html_attrib/parishStyles/services/servbootstrap.css" rel = "stylesheet"><link href = "'+ base_url +'html_attrib/parishStyles/css/styles.css" rel = "stylesheet"><link rel="stylesheet" type="text/css" href="'+ base_url +'html_attrib/parishStyles/css/parishStyle.css" media="screen"></style>';
+		document.getElementById("init").innerHTML = '<meta name ="viewport" content = "width=device-width, initial-scale = 1.0"><link href = "'+ base_url +'html_attrib/parishStyles/css/bootstrap_2.css" rel = "stylesheet"><link href = "'+ base_url +'html_attrib/parishStyles/css/newStyle.css" rel = "stylesheet"><link href = "'+ base_url +'html_attrib/parishStyles/css/styles.css" rel = "stylesheet"><link rel="stylesheet" type="text/css" href="'+ base_url +'html_attrib/parishStyles/css/parishStyle.css" media="screen"></style>';
 	};
 };
 
